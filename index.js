1b3e72582ea98ddd922c6c7d3397bfe150b9b9f5
kaverapp <138514864+kaverapp@users.noreply.github.com>
2024-09-04 08:06:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+let api = "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";
+
+let prompt=require("prompt-sync")();
+
+
+
+
+let gitFetch = async (Owner) => {
+    let response = await fetch(`https://api.github.com/users/${Owner}/events`, {
+        headers: {
+            'Authorization': `token ${api}`
+        }
+    });
+
+    if (response.ok) {
+        let data = await response.json();
+        
+        // Map through the events and return a formatted string
+        let formattedEvents = data.map(event => {
+            let eventType = event.type;
+            let repoName = event.repo.name;
+            
+            
+
+            if (eventType === "PushEvent") {
+                let commitCount = event.payload.commits.length;
+                return `Pushed ${commitCount} commit(s) to ${repoName}`;
+            } else if (eventType === "IssuesEvent" && event.payload.action === "opened") {
+                return `Opened a new issue in ${repoName}`;
+            } else if (eventType === "WatchEvent") {
+                return `Starred ${repoName}`;
+            } else {
+                return `${eventType} in ${repoName}`;
+            }
+        }
+       
+    ); 
+    
+
+        // Log the formatted events
+        formattedEvents.forEach(event => console.log(event));
+
+    } else {
+        console.error('Error:', response.status, response.statusText);
+    }
+    
+
+};
+
+let user=prompt("enter the login id")
+gitFetch(user);
+
+
+
+
+
